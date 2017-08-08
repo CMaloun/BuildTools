3d51cdbc213c7331d8b5d469876f04c0ba662274
MALOUNGILA <katsoupitsou@hotmail.com>
2017-08-08 16:37:19 +0200

    build script sh

---
+++ packer/build-vm-image.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+rm packer-build-output.log
+
+echo "************* execute packer build"
+## execute packer build and sendout to packer-build-output file
+packer build ./azure/linux/azure_linux_web.packer 2>&1 | tee packer-build-output.log

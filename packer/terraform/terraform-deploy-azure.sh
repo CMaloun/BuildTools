9bd2afffab49a1aca70e89825019bb7c5773df68
MALOUNGILA <katsoupitsou@hotmail.com>
2017-08-09 15:38:41 +0200

    modify terrform.sh file

--- packer/terraform/terraform-deploy-azure.sh
+++ packer/terraform/terraform-deploy-azure.sh
@@ -3,5 +3,7 @@
 rm terraform-build-output.log
 
 echo "************* execute terraform build"
+terraform init
+
 ## execute terraform build and sendout to terraform-build-output file
 terraform apply 2>&1 | tee terraform-build-output.log

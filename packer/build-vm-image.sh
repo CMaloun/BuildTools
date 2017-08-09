e107839468961e0d4384f675a2480f95105b439d
MALOUNGILA <katsoupitsou@hotmail.com>
2017-08-09 10:28:43 +0200

    change build-vm-image.sh

--- packer/build-vm-image.sh
+++ packer/build-vm-image.sh
@@ -4,4 +4,4 @@ rm ./logs/packer-build-output.log
 
 echo "************* execute packer build"
 ## execute packer build and sendout to packer-build-output file
-packer build ./azure/linux/azure_linux_web.packer 2>&1 | tee ./logs/packer-build-output.log
+packer build ./azure/linux/azure_linux_web.packer 2>&1 | tee packer-build-output.log

08660da7e76e7b1ada5ac124f310c1395ffa840a
MALOUNGILA <katsoupitsou@hotmail.com>
2017-08-09 12:19:24 +0200

    add terraform files

--- packer/terraform/overwrite-imageid-terraform.sh
+++ packer/terraform/overwrite-imageid-terraform.sh
@@ -3,3 +3,7 @@ cd ..#!/bin/bash
 export imageodisk=$(cat ../../Packer/Logs/packer-build-output.log | grep OSDiskUri: | awk '{print $2}')
 
 echo $imageodisk
+
+sed -i 's|@@IMAGEURI@@|'"$imageodisk"'|g' main.tf
+
+cat maint.tf
